7c160b2664ffe7019088b130bed823c99633035c
Lasha Kantaria <lasha.kantaria@hotmail.com>
2022-01-07 14:06:42 +0400

    update

--- src/app/components/sub-version/sub-version.component.ts
+++ src/app/components/sub-version/sub-version.component.ts
@@ -18,11 +18,11 @@ export class SubVersionComponent implements OnInit {
     if (this.version.type == 'primary') {
       return 'Initial value of major set to ' + this.version.data.currentMajor + ', Initial value of minor set to ' + this.version.data.currentMinor;
     } else if (this.version.type == 'nextMinor') {
-      return 'minor changed from ' + this.version.data.previousMinor + ' to ' + this.version.data.currentMinor;
+        return 'minor changed from ' + this.version.data.previousMinor + ' to ' + this.version.data.currentMinor;
     } else if (this.version.type == 'nextMajor') {
-      return 'major changed from ' + this.version.data.previousMajor + ' to ' + this.version.data.currentMajor;
+        return 'major changed from ' + this.version.data.previousMajor + ' to ' + this.version.data.currentMajor;
     } else {
-      return 'Error!';
+        return 'Error!';
     }
   }
 }
